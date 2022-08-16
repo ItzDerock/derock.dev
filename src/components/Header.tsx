b3f8512a6ac3a04ae99754962f41d2d29c246076
Derock <derock@derock.dev>
2022-08-16 17:55:57 -0400

    feat: spice up navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,51 @@
 import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import { Fragment } from "react";
 import { useTime } from "../hooks/Time";
 import { FontAwesomeLink } from "./Links/IconLink";
+import { NavbarElement } from "./Navbar/NavbarElement";
+
+const pages = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Reviews",
+    href: "/reviews",
+  }
+];
 
 export function Header() {
   const time = useTime(5000);
 
   return (
     <header>
-      <div className="grid grid-cols-3 gap-3 px-3 bg-header font-header">
-        <div className="col-start-1 col-span-1 text-left text-white">
-          <div className="flex items-center h-full gap-3">
+      <div className="grid grid-cols-3 gap-3 px-1 bg-header font-header">
+        <div className="col-start-1 col-span-1 text-left text-white items-center">
+          {/* navbar */}
+          {
+            pages.map(({ name, href }, i) => (
+              i === pages.length - 1 ? (
+                <NavbarElement key={i} name={name} link={href} />
+              ) : (
+                <Fragment>
+                  <NavbarElement key={i} name={name} link={href} />
+                  <span className="text-dots">|</span>
+                </Fragment>
+              )
+            ))
+          }
+
+        </div>
+        <div className="col-start-2 col-span-1 text-center text-white items-center">
+          <p className="border-b-2 duration-200 hover:px-4 hover:cursor-pointer px-1 m-auto w-fit border-primary text-primary">
+            <Link href="/">derock.dev</Link>
+          </p>
+        </div>
+        <div className="col-start-3 col-span-1 text-right text-white">
+          <div className="flex items-center h-full gap-3 text-right w-fit float-right pr-2">
             <FontAwesomeLink
               icon={faDiscord}
               to="https://discord.com"
@@ -26,20 +61,16 @@ export function Header() {
               to="mailto:derock@derock.dev"
               tooltip="Email"
             />
+
+            <span className="text-dots"> | </span>
+
+            <h1>
+              {
+                // time in 24-hour format HH:MM
+                time.getHours().toString().padStart(2, "0") + ":" + time.getMinutes().toString().padStart(2, "0")
+              }
+            </h1>
           </div>
-        </div>
-        <div className="col-start-2 col-span-1 text-center text-white items-center">
-          <p className="border-b-2 duration-200 hover:px-4 hover:cursor-pointer px-1 m-auto w-fit border-primary text-primary">
-            <a href="https://derock.dev">derock.dev</a>
-          </p>
-        </div>
-        <div className="col-start-3 col-span-1 text-right text-white">
-          <h1>
-            {
-              // time in 24-hour format HH:MM
-              time.getHours().toString().padStart(2, "0") + ":" + time.getMinutes().toString().padStart(2, "0")
-            }
-          </h1>
         </div>
       </div>
     </header>
---
+++ src/components/Navbar/NavbarElement.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export function NavbarElement({ name, link }: { name: string, link: string }) {
+  const router = useRouter();
+  const isActive = router.pathname === link;
+  
+  return (
+    <div className={
+      (isActive ? "text-primary bg-slate-600 border-primary border-b-2" : "")
+      + " inline-block px-5"
+    }>
+      <Link href={link}>{name}</Link>
+    </div>
+  )
+}
